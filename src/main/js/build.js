95de42471d5d1fff120f435244a39c6df4e5bab1
Imre Tabur <imre.tabur@eesti.ee>
2017-11-30 22:01:54 +0200

    For older version of nodejs

--- src/main/js/build.js
+++ src/main/js/build.js
@@ -7,12 +7,9 @@ let options = {
     }
 };
 
+
 fs.writeFileSync("dist/servicejs.min.js", uglify.minify({
     "servicejs.js": fs.readFileSync("src/main/webapp/js/servicejs.js", "utf8")
 }, options).code, "utf8");
 
-fs.copyFile('src/main/webapp/js/servicejs.js', 'dist/servicejs.js', (err) => {
-    if (err) {
-        throw err;
-    }
-});
+fs.createReadStream('src/main/webapp/js/servicejs.js').pipe(fs.createWriteStream('dist/servicejs.js'));
