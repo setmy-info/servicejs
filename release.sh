ef513ef62cd0408ea1a89c6ad2ecc09ecb75af48
Imre Tabur <imre.tabur@eesti.ee>
2017-12-19 08:14:27 +0200

    version-1.0.29 development started

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 # MIT License
 # Copyright (c) 2017 Imre Tabur <imre.tabur@eesti.ee>
 
-RELEASE=1.0.28
+RELEASE=1.0.29-SNAPSHOT
 VERSION_TAG=version-${RELEASE}
 
 git add pom.xml package.json release.sh
