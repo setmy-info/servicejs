47f06186301a0b3500038ed39c35354cabdaa0f9
Imre Tabur <imre.tabur@eesti.ee>
2019-04-25 21:31:34 +0300

    version-1.1.1

--- release.sh
+++ release.sh
@@ -3,11 +3,11 @@
 # MIT License
 # Copyright (c) 2017-2019 Imre Tabur <imre.tabur@eesti.ee>
 
-RELEASE=1.1.0
+RELEASE=1.1.1
 VERSION_TAG=version-${RELEASE}
 
 npm install && npm run build && npm run test
-git add package.json release.sh package-lock.json
+git add ./dist package.json release.sh package-lock.json
 git commit -m "${VERSION_TAG}"
 git push
 git checkout master
