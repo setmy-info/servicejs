01b178ab2cdfcad4895c9bd5ff251f61f66a146b
Imre Tabur <imre.tabur@eesti.ee>
2019-05-01 18:33:00 +0300

    License info added

--- src/build/index.js
+++ src/build/index.js
@@ -1,9 +1,18 @@
 var fs = require('fs');
 var uglify = require("uglify-js");
 
-fs.writeFileSync("./src/frontend/public/js/servicejs.min.js", uglify.minify({
+
+var code = {
     "servicejs.js": fs.readFileSync("./src/frontend/public/js/servicejs.js", "utf8")
-}, {}).code, "utf8");
+};
+
+var options = {
+    output: {
+        comments: /^!/
+    }
+};
+
+fs.writeFileSync("./src/frontend/public/js/servicejs.min.js", uglify.minify(code, options).code, "utf8");
 
 fs.createReadStream("./src/frontend/public/js/servicejs.js").pipe(fs.createWriteStream("./dist/servicejs.js"));
 fs.createReadStream("./src/frontend/public/js/servicejs.min.js").pipe(fs.createWriteStream("./dist/servicejs.min.js"));
