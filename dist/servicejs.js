e5d2be584de9a001920b882486d63974ce0037c9
Imre Tabur <imre.tabur@eesti.ee>
2019-05-01 18:38:56 +0300

    version-1.1.6

--- dist/servicejs.js
+++ dist/servicejs.js
@@ -21,9 +21,8 @@
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.
 
-    https://github.com/Krabi/servicejs
- 
- */
+    https://github.com/setmy-info/servicejs
+*/
 "use strict";
 
 (function (global) {
